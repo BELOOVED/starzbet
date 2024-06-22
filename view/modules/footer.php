5c11c5c3eef1be1584ba3561388fd0809a9b7e88
Beloved <aycahanim@gmail.com>
2024-06-22 14:28:53 +0300

    aaa

--- view/modules/footer.php
+++ view/modules/footer.php
@@ -1047,59 +1047,7 @@
    </div>
 </div>
 <script>
-    function deopa(){
-      $(".dropdown-wrapper").removeClass("inactive");
-      $(".dropdown-wrapper").removeClass("is-active");
-
-   }
-   function guncelleniyor(){
-      swal.fire('Hata!','Bu ödeme yöntemi güncelleniyor','warning');
-   }
-   function yetersiz(){
-      swal.fire('Hata!','Yetersiz Bakiye, Canlı Desteke İletişime Geçiniz','warning');
-   }
-   function openmodal(id){
-  closesmodal();
-
-  $("#"+id).removeClass("hidden");
-  $(".modal--Pplvp").removeClass("hidden");
-
-}
-function closesmodal(){
-   $(".full-screen-container--tDOkN").addClass("hidden");
-
-   $(".modal--Pplvp").addClass("hidden");
-}
-function closemodal(){
-   $(".modal--with-banner").addClass("hidden");
-}
-function backModal(id){
-  if (id === "notifications" || id === "favourites"|| id === "settings") {
-  closemodal();
-} 
-
-else{
-  closemodal();
-  openmodal('profile');
-}
-}
-function openusermenu(){
-  $("#user-settings").toggleClass("active");
-} 
-function openbalancemenu(){
-  $("#balance-menu").toggleClass("active");
-} 
-function openmodals(id){
-  closemodals();
-  $(".user-profile-container").removeClass("hidden");
-   
-  $("#"+id).removeClass("hidden");
-  
-}
-function closemodals(id){
-  $(".my-profile-info-block").addClass("hidden");
-  
-}
+    
 </script>
 </body>
 </html>

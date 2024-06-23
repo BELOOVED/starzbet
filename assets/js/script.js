1c9679a0c971aefb05cd5c05d73a522b5214652f
Beloved <aycahanim@gmail.com>
2024-06-23 21:18:04 +0300

    sss

--- assets/js/script.js
+++ assets/js/script.js
@@ -181,9 +181,7 @@ $(document).ready(function() {
      slidesToShow: 1,
      slidesToScroll: 1,
      swipeToSlide: true,
-     customPaging: function(slick, index) {
-       return '<a class="slick-dots" href="#"><svg class="slider-loader" viewBox="0 0 33.83098862 33.83098862" width="26" height="26" xmlns="http://www.w3.org/2000/svg"><circle class="slider-loader__background" stroke="#767A85" stroke-width="2" fill="none" cx="16.91549431" cy="16.91549431" r="15.91549431"></circle><circle class="slider-loader__circle" stroke="#FFD60E" stroke-width="2.5" stroke-dasharray="100" stroke-linecap="round" fill="none" cx="16.91549431" cy="16.91549431" r="15.91549431"></circle></svg></a>';
-     }
+     
    });
  
    $('.slider-slickone').on('afterChange', function(event, slick, currentSlide) {
